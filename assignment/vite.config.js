28863bd41c183974e58fd1530c3c58e0173a49ba
Rithy <uthchanrithy.wu@gmail.com>
2026-06-30 20:47:01 +0700

    Fix Vite base path for GitHub Pages

--- assignment/vite.config.js
+++ assignment/vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Assignment/", 
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
   ],
-})
+});
